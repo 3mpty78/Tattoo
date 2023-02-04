1a4eff2aa6a957097188ca29201a204a6f5cbf8b
3mpty <aurelien.corre@outlook.fr>
2023-02-04 23:10:28 +0100

    Design Home.js

--- tattoo-roulette/src/Pages/Home.js
+++ tattoo-roulette/src/Pages/Home.js
@@ -6,20 +6,24 @@ const Home = () => {
     <>
       <main>
         <Header />
-        <h2>
-          Find the perfect <strong>design</strong>, <br />
-          We find the perfect <span>artist</span>.
-        </h2>
-        <article>
-          <h3>Freaks And Geeks</h3>
-          <h4>Salon à Paris</h4>
-          <img src="" alt="Salon Freaks and Geeks à Paris" />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo esse
-            perspiciatis impedit quo iure dolore animi velit facilis, ex
-            assumenda.
-          </p>
-        </article>
+        <section className="welcome">
+          <h2>
+            Find the perfect <strong>design</strong>, <br />
+            We find the perfect <span>artist</span>.
+          </h2>
+        </section>
+        <section className="cards">
+          <article>
+            <div className="freaksPhoto"></div>
+            <h3>Freaks And Geeks Tattoo</h3>
+            <h4>Salon à Paris</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo esse
+              perspiciatis impedit quo iure dolore animi velit facilis, ex
+              assumenda.
+            </p>
+          </article>
+        </section>
       </main>
     </>
   );
