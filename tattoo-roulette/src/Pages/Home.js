ab2c9e00181e0ee15cc6de6a9d2c9b63f2cefa12
3mpty <aurelien.corre@outlook.fr>
2023-02-05 19:31:09 +0100

    test

--- tattoo-roulette/src/Pages/Home.js
+++ tattoo-roulette/src/Pages/Home.js
@@ -17,7 +17,7 @@ function Home() {
           <div className="panel bottom">
             <div className="content">
               <div className="description">
-                <h2>We offer you artists</h2>
+                <h2>We find artists</h2>
               </div>
               <div className="OriginalPicture"></div>
             </div>
