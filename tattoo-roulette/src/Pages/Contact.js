7e00509b8bac70d10a0312238b56059a50721857
3mpty <aurelien.corre@outlook.fr>
2023-03-02 19:12:31 +0100

    Style Contact.js started

--- tattoo-roulette/src/Pages/Contact.js
+++ tattoo-roulette/src/Pages/Contact.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
 
 const Contact = () => {
-  return <div></div>;
+  return (
+    <>
+      <main id="contact">
+        <Header />
+        <Footer />
+      </main>
+    </>
+  );
 };
 
 export default Contact;
